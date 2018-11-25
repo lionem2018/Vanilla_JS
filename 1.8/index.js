d1e6cd9292cc6ec79ea19d084f8ecb6065d73ca5
lionem2018 <lionem2018@gmail.com>
2018-11-25 19:27:19 +0900

    1.8 Data Types on JS

---
+++ 1.8/index.js
@@ -0,0 +1,18 @@
+// Strig
+const what = "Nicolas";
+// const what = Nicolas; (occur error)
+// const what = "59595959" (not number, String)
+
+// Boolean
+const wat = true;
+
+// Number
+const wat2 = 666;
+
+// Float
+const wat3 = 55.1
+
+console.log(what);
+console.log(wat);
+console.log(wat2);
+console.log(wat3);
