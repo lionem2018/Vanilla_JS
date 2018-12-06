9a1f0940fffc45ac6ae9b8d96e2fab401e1eff66
lionem2018 <lionem2018@gmail.com>
2018-12-06 19:58:25 +0900

    1.9 Organizing Data with Arrays

---
+++ 1.9/index.js
@@ -0,0 +1,11 @@
+// const monday = "Mon";
+// const tue = "Tue";
+// const wed = "Wed";
+// const thu = "Thu";
+// const fri = "Fri";
+const something = "Something"
+const daysOfWeek = [ "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun",
+                    54, true, something ];
+
+console.log(daysOfWeek);
+console.log(daysOfWeek[2]);
