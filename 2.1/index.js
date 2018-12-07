8341eba0baebfe2c8a76624f0f35abea1de6f333
lionem2018 <lionem2018@gmail.com>
2018-12-07 23:57:35 +0900

    2.1.1 More Function Fun

--- 2.1/index.js
+++ 2.1/index.js
@@ -14,5 +14,38 @@
 function sayHello(name, age){
     console.log('Hello!', name, "you have", age, "years of age.");
 }
-
 sayHello("Nicolas", 15);    // name = "Nicolas", age = 15
+
+function sayHello2(name, age){
+    return `Hello ${name} you are ${age} years old`;
+}
+const greetNicolas = sayHello2("Nicolas", 14);
+console.log(greetNicolas)
+
+const calculator = {
+    plus: function(a, b){
+        return a + b;
+    },
+    minus: function(a, b){
+        return a - b;
+    },
+    multiply: function(a, b){
+        return a * b;
+    },
+    divide: function(a, b){
+        return a / b;
+    },
+    power: function(a, b){
+        return a ** b;
+    }
+}
+const plus = calculator.plus(5, 5);
+const minus = calculator.minus(5, 5);
+const multiply = calculator.multiply(5, 5);
+const divide = calculator.divide(5, 5);
+const power = calculator.power(5, 5);
+console.log(plus);
+console.log(minus);
+console.log(multiply);
+console.log(divide);
+console.log(power);
