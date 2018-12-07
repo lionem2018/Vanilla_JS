de18a7d50b8da1b38096e5f5a2bffab0379af189
lionem2018 <lionem2018@gmail.com>
2018-12-07 23:43:09 +0900

    2.1 Your first JS Function

---
+++ 2.1/index.js
@@ -0,0 +1,18 @@
+// const nicoInfo = {
+//     name: "Nico",
+//     age: 33,
+//     gender: "Male",
+//     isHandsome: true
+// }
+// console.log(nicoInfo, console);
+
+// console.log('Gettings Nicolas');
+// console.log('Hello Jun');
+// console.log('Hello Liyn');
+// console.log('Hello Dal');
+
+function sayHello(name, age){
+    console.log('Hello!', name, "you have", age, "years of age.");
+}
+
+sayHello("Nicolas", 15);    // name = "Nicolas", age = 15
